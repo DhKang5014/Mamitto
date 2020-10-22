b5f9ac43395beb271c89f831dc07b0302a67d706
SMT010 <SMT010@DESKTOP-E2VURPK>
2020-10-22 17:24:32 +0900

    1022 us 1720 commit

--- WebContent/script/script_us.js
+++ WebContent/script/script_us.js
@@ -395,7 +395,7 @@ function check() {
 					  $('#id_check_on').css('display','block'); 
 					  $('#id_check_off').css('display','none');
 					}
-		           });		           
+		           });
 		           
 		    }) // HTTP 요청이 실패하면 오류와 상태에 관한 정보가 fail() 메소드로 전달됨. 
 		    .fail(function(xhr, status, errorThrown) { 
