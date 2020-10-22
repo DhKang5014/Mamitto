6aafed4711c80765a90a28cf247317319962150a
annedazh <67046331+annedazh@users.noreply.github.com>
2020-10-23 02:04:21 +0900

    dh comm 1400

---
+++ WebContent/public/dddd.js
@@ -0,0 +1,53 @@
+
+
+
+        var firebaseConfig = {
+            apiKey: "AIzaSyABALGEjxe6qKFf8tmKmKtiDJggsO4Hvho",
+            authDomain: "mamiddo-cebd2.firebaseapp.com",
+            databaseURL: "https://mamiddo-cebd2.firebaseio.com",
+            projectId: "mamiddo-cebd2",
+            storageBucket: "mamiddo-cebd2.appspot.com",
+            messagingSenderId: "228735732994",
+            appId: "1:228735732994:web:bd0f5eeaaae5872951c61b",
+            measurementId: "G-SY1N03SC7H"
+        };
+
+
+
+
+
+        firebase.initializeApp(firebaseConfig);
+    const messaging= firebase.messaging();
+
+function InitializeFireBaseMessaging(){
+    messaging.requestPermission()
+    .then(function(){
+        console.log("Notification Permission");
+    })
+    .then(function(token){
+        console.log("Token : " + token);
+        document.getElementById('token').innerHTML = token;
+    }).catch(function(reason){
+        console.log(reason);
+    });
+}
+
+messaging.onMessage(function (payload){
+    console.log(payload);
+});
+
+
+messaging.onTokenRefresh(function(){
+
+    messaging.getToken().then(function(newtoken){
+
+    console.log("New Token : " + newtoken);
+
+})
+    .catch(function(reason){
+        console.log(reason);
+    });
+
+});
+
+InitializeFireBaseMessaging();
